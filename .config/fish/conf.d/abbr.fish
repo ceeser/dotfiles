66d2f51e9a80b1c7a0e6b03b24289bb188c6a9d6
ceeser <1509497+ceeser@users.noreply.github.com>
2024-02-01 23:14:23 -0500

    add fish configs

---
+++ .config/fish/conf.d/abbr.fish
@@ -0,0 +1,45 @@
+# Convenience
+abbr ~ "cd ~"
+abbr .. 'cd ../'         # Go back 1 directory level
+abbr ... 'cd ../../'     # Go back 2 directory levels
+abbr c "clear"
+abbr cp "cp -iv"         # Preferred 'cp' implementation
+abbr l "ls -lah"
+abbr mv "mv -iv"         # Preferred 'mv' implementation
+abbr mkdir "mkdir -pv"   # Preferred 'mkdir' implementation
+
+# Applications
+abbr cat "bat"
+# abbr docker "podman"
+# abbr dockerremovevolumesdangling "docker volume rm $(docker volume ls -f dangling=true -q)"
+# abbr dockerpruneall "docker container prune -f && docker system prune -f && docker volume rm $(docker volume ls -f dangling=true -q)"
+abbr glg "git lg"
+abbr glg1 "git lg1-specific --all"
+abbr glg2 "git lg2-specific --all"
+abbr glg3 "git lg3-specific --all"
+abbr gm "git merge"
+abbr gpr "git pull --rebase"
+abbr gs "git status"
+abbr tmd "tmux attach -t dev || tmuxinator dev"
+abbr tmk "tmux kill-session -t dev"
+abbr vi "vim"
+
+# Dotfiles Config
+abbr config "/run/current-system/sw/bin/git --git-dir=$HOME/.myconfig/ --work-tree=$HOME"
+
+# Git
+abbr gs = "git status"
+
+# Nix commands
+abbr nec "vim ~/configuration.nix" # edit home config
+abbr ndg "sudo nix-env --delete-generations --profile /nix/var/nix/profiles/system" # delete specified generations
+abbr ngc "sudo nix-collect-garbage --delete-old"
+abbr nlg "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system" # list all generations
+abbr nr "cd ~/ && sudo nixos-rebuild switch -I nixos-config=configuration.nix" # rebuild from home config
+abbr nrollb "sudo nix-env --rollback --profile /nix/var/nix/profiles/system" # rollback to previous gen
+abbr nsg "sudo nix-env --switch-generation --profile /nix/var/nix/profiles/system" # switch to specified gen
+abbr nu "cd ~/ && sudo nixos-rebuild switch --upgrade -I nixos-config=configuration.nix" # update to latest packages
+
+# Misc
+abbr su "su -l"
+abbr sudo "sudo " # fix for recognizing aliases when sudoing
---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+# Imports
+source $HOME/.config/fish/conf.d/abbr.fish
+
+# Env Vars
+set -gx EDITOR vim
+set -gx OS $(uname)
+
+set -gx LANG en_US.UTF-8
+set -gx LANGUAGE en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
+set -gx LC_COLLATE C
+set -gx LC_CTYPE en_US.UTF-8
+
+set -gx HOMEBREW_NO_ANALYTICS 1
+set -gx HOMEBREW_NO_INSECURE_REDIRECT 1
+set -gx HOMEBREW_CASK_OPTS --require-sha
+
+set -gx BLOCKSIZE 1k
+set -gx TERM xterm-256color
+set -gx COLUMNS 120
+
+starship init fish | source
