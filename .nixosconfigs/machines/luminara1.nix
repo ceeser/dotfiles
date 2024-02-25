5c7ae80006a9b06cf024bb40e5609ba6b2a483f5
ceeser <1509497+ceeser@users.noreply.github.com>
2024-02-25 02:22:49 -0500

    add luminara:wq

---
+++ .nixosconfigs/machines/luminara1.nix
@@ -0,0 +1,21 @@
+{ lib, config, pkgs, baseMachineTypePackages, baseMachineTypeServices }:
+
+{
+  #imports = [];
+  #boot = {};
+
+  environment.systemPackages = with pkgs; [
+    librewolf
+  ] ++ baseMachineTypePackages;
+
+  networking.hostName = "luminara1"; # Define your hostname.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+
+  services = lib.recursiveUpdate baseMachineTypeServices {
+    resolved.enable = false;
+  };
+
+  time.timeZone = "America/Toronto";
+}
