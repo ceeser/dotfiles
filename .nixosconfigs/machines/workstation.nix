0624940ffa39554276c747130e2828d67a3bd9d5
ceeser <1509497+ceeser@users.noreply.github.com>
2024-06-13 21:36:22 -0400

    add lazygit

--- .nixosconfigs/machines/workstation.nix
+++ .nixosconfigs/machines/workstation.nix
@@ -99,6 +99,7 @@ in {
 
         ## development
         diff-so-fancy
+        lazygit
         #dive # look into docker image layers
         #podman-tui # status of containers in the terminal
         #podman-compose # start group of containers for dev
