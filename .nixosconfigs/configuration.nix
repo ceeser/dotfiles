37f4d08b0e65f85a46bbe12d3cfe66ff20a0ae6a
ceeser <1509497+ceeser@users.noreply.github.com>
2024-06-13 22:19:00 -0400

    Add xdg vars

--- .nixosconfigs/configuration.nix
+++ .nixosconfigs/configuration.nix
@@ -51,6 +51,20 @@ in {
 
   documentation.nixos.enable = false;
 
+  environment.sessionVariables = rec {
+    XDG_CACHE_HOME  = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME   = "$HOME/.local/share";
+    XDG_STATE_HOME  = "$HOME/.local/state";
+
+    # Not officially in the specification
+    XDG_BIN_HOME    = "$HOME/.local/bin";
+    PATH = [ 
+      "${XDG_BIN_HOME}"
+    ];
+  };
+
+
   # Fonts
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "FiraCode" ]; })
