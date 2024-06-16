6ee764aac96092fdf4c6e1cbc79851582fe192a0
ceeser <1509497+ceeser@users.noreply.github.com>
2024-06-16 18:55:23 -0400

    add power settings

--- .nixosconfigs/machines/workstations/tera.nix
+++ .nixosconfigs/machines/workstations/tera.nix
@@ -12,6 +12,8 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
 
+  powerManagement.powertop.enable = true;
+
   services = lib.recursiveUpdate baseMachineTypeServices {
     power-profiles-daemon.enable = false;
     syncthing = {
@@ -22,7 +24,25 @@
       overrideDevices = true;     # overrides any devices added or deleted through the WebUI
       overrideFolders = true;     # overrides any folders added or deleted through the WebUI
     };
-    tlp.enable = true;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "power";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 50;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 20;
+
+       #Optional helps save long term battery health
+       START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+       STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      };
+    };
   };
 
   time.timeZone = "America/Toronto";
