368eb35cb6e02c1dbe23f87a99567265abc11ea0
ceeser <1509497+ceeser@users.noreply.github.com>
2024-10-13 23:02:04 -0400

    update dash

--- .nixosconfigs/services/glance-dash.nix
+++ .nixosconfigs/services/glance-dash.nix
@@ -1,82 +1,36 @@
-# Config for glance dashbaord
-
-{ config, pkgs, ... }:
-
-{
-  services.glance = {
-    enable = true;
-    settings = {
-      server = {
-        base-url = "/home";
-        host = "0.0.0.0";
-        port = 38295;
-      };
-      branding = {
-        hide-footer = true;
-        logo-text = "C";
-      };
-      theme = {
-        light = false;
-        background-color = "hsl(240, 21%, 15%)";
-        contrast-multiplier = 1.2;
-        primary-color = "hsl(217 92% 83%)";
-        positive-color = "hsl(115 54% 76%)";
-        negative-color = "hsl(347 70% 65%)";
-      };
-      pages = [
-        {
           columns = [
             {
               size = "small";
               widgets = [
                 {
-                  type = "bookmarks";
-                  groups = [
-                    {
-                      links = [
-                        { title = "Books"; url = "https://aayla3.bun-buri.ts.net/calibre/";}
-                        { title = "Documents"; url = "https://aayla3.bun-buri.ts.net/paperless/accounts/login/?next=/paperless/";}
-                        { title = "Passwords"; url = "https://aayla3.bun-buri.ts.net/vaultwarden/#/login";}
-                        { title = "Photos"; url = "https://photos.ceeser.com/";}
-                        { title = "Sync"; url = "http://127.0.0.1:8384/#";}
-                      ];
-                    }
-                    {
-                      title = "Finance";
-                      color = "hsl(10 70% 50%)";
-                      links = [
-                        { title = "Carta"; url = "https://app.carta.com/accounts/login";}
-                        { title = "Chase"; url = "https://secure.chase.com/web/auth/dashboard#/dashboard/index/index";}
-                        { title = "EQ Bank"; url = "https://secure.eqbank.ca/login";}
-                        { title = "Kraken"; url = "https://www.kraken.com/sign-in";}
-                        { title = "N26"; url = "https://app.n26.com/login";}
-                        { title = "Neo"; url = "https://member.neofinancial.com/login";}
-                        { title = "Privacy"; url = "https://privacy.com/home";}
-                        { title = "Robinhood"; url = "https://robinhood.com/us/en/";}
-                        { title = "Splitwise"; url = "https://www.splitwise.com/";}
-                        { title = "TD"; url = "https://www.td.com/ca/en/personal-banking/";}
-                        { title = "WealthSimple"; url = "https://www.wealthsimple.com/en-ca/";}
-                        { title = "Wise"; url = "https://wise.com/";}
-                      ];
-                    }
-                    {
-                      title = "Tech";
-                      color = "hsl(200 50% 50%)";
-                      links = [
-                        { title = "Oracle Cloud"; url = "https://cloud.oracle.com";}
-                      ];
-                    }
+                  type = "markets";
+                  title = "Indices";
+                  markets = [
+                    { symbol = "SPY"; name = "S&P 500";}
+                    { symbol = "DX-Y.NYB"; name = "Dollar Index";}
                   ];
                 }
                 {
                   type = "markets";
+                  title = "Crypto";
                   markets = [
-                    { name = "S&P 500"; symbol = "SPY"; }
+                    { symbol = "BTC-USD"; name = "Bitcoin";}
+                    { symbol = "ETH-USD"; name = "Ethereum";}
+                  ];
+                }
+                {
+                  type = "markets";
+                  title = "Stocks";
+                  sort-by = "absolute-change";
+                  markets = [
+                    { name = "AMD"; symbol = "AMD"; }
+                    { name = "Amazon"; symbol = "AMZN"; }
                     { name = "Apple"; symbol = "AAPL"; }
                     { name = "Google"; symbol = "GOOGL"; }
-                    { name = "Nvidia"; symbol = "NVDA"; }
-                    { name = "AMD"; symbol = "AMD"; }
                     { name = "Meta"; symbol = "META"; }
+                    { name = "Microsoft"; symbol = "MSFT"; }
+                    { name = "Nvidia"; symbol = "NVDA"; }
+                    { name = "Tesla"; symbol = "TSLA"; }
                   ];
                 }
               ];
@@ -86,34 +40,57 @@
               widgets = [
                 {
                   type = "rss";
+                  title = "News";
                   style = "horizontal-cards";
-                  limit = 20;
                   feeds = [
-                    { title = "The Grumpy Economist"; url = "https://www.grumpy-economist.com/feed"; }
-                    { title = "Julia Evans"; url = "https://jvns.ca/atom.xml"; }
+                    { title = "Bloomberg"; url = "https://feeds.bloomberg.com/markets/news.rss"; }
+                    { title = "Fox Business"; url = "https://moxie.foxbusiness.com/google-publisher/markets.xml"; }
+                    { title = "Fox Business"; url = "https://moxie.foxbusiness.com/google-publisher/technology.xml"; }
                   ];
                 }
-                { type = "hacker-news"; collapse-after = 30; }
+                {
+                  type = "group";
+                  widgets = [
+                    {
+                      type = "reddit";
+                      show-thumbnails = true;
+                      subreddit = "technology";
+                    }
+                    {
+                      type = "reddit";
+                      show-thumbnails = true;
+                      subreddit = "wallstreetbets";
+                    }
+                  ];
+                }
+                {
+                  type = "videos";
+                  style = "grid-cards";
+                  collapse-after-rows = 3;
+                  channels = [
+                    "UCvSXMi2LebwJEM1s4bz5IBA" # New Money
+                    "UCV6KDgJskWaEckne5aPA0aQ" # Graham Stephan
+                    "UCAzhpt9DmG6PnHXjmJTvRGQ" # Federal Reserve
+                  ];
+                }
               ];
             }
             {
               size = "small";
               widgets = [
-                { type = "calendar"; }
                 {
-                  type = "clock";
-                  hour-format = "24h";
-                  timezones = [
-                    { label = "San Francisco"; timezone = "America/Los_Angeles"; }
-                    { label = "New York"; timezone = "America/New_York"; }
-                    { label = "London"; timezone = "Europe/London"; }
-                    { label = "Melbourne"; timezone = "Australia/Melbourne"; }
+                  type = "rss";
+                  title = "News";
+                  limit = 30;
+                  collapse-after = 13;
+                  feeds = [
+                    { title = "Financial Times"; url = "https://www.ft.com/technology?format=rss"; }
+                    { title = "Fox Business"; url = "https://feeds.a.dj.com/rss/RSSMarketsMain.xml"; }
                   ];
                 }
               ];
             }
           ];
-          name = "Home";
         }
       ];
     };
