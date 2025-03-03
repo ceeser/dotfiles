e9ab45c63b0b24c62edf4f9c751f11affb4388c0
ceeser <1509497+ceeser@users.noreply.github.com>
2025-03-03 12:13:20 -0500

    disable exif tool in photoprism

--- .nixosconfigs/machines/servers/aayla3.nix
+++ .nixosconfigs/machines/servers/aayla3.nix
@@ -89,6 +89,7 @@
         PHOTOPRISM_DISABLE_BACKUPS = "false";
         PHOTOPRISM_DISABLE_CHOWN = "true";                    # disables updating storage permissions via chmod and chown on startup
         PHOTOPRISM_DISABLE_CLASSIFICATION = "true";           # disables image classification (requires TensorFlow)
+        PHOTOPRISM_DISABLE_EXIFTOOL = "true";
         PHOTOPRISM_DISABLE_FACES = "true";                    # disables face detection and recognition (requires TensorFlow)
         PHOTOPRISM_DISABLE_PLACES = "true";
         PHOTOPRISM_DISABLE_RAW = "false";                     # disables indexing and conversion of RAW images
