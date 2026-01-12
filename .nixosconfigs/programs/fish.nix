a39eebd1713a2c80a697295f83db360aca4d8b34
ceeser <1509497+ceeser@users.noreply.github.com>
2026-01-11 23:15:49 -0500

    add vim mode to fish

--- .nixosconfigs/programs/fish.nix
+++ .nixosconfigs/programs/fish.nix
@@ -17,6 +17,7 @@
       enable = true;
       interactiveShellInit = ''
         # set options
+        set -g fish_key_bindings fish_vi_key_bindings
         set fish_greeting
 
         # Env Vars
