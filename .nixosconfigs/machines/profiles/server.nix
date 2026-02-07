d647c37f360878a42a1c63cc85b3c6671b8aed65
ceeser <1509497+ceeser@users.noreply.github.com>
2026-02-07 16:06:24 -0500

    remove old key

--- .nixosconfigs/machines/profiles/server.nix
+++ .nixosconfigs/machines/profiles/server.nix
@@ -73,7 +73,6 @@
         initialHashedPassword = "$y$j9T$XaUpu.uuIDnfAYfzPxKqW.$LF6faepJC1x3F2akjhbv0fUkl314I7E3XmVekKL7TBA";
         isNormalUser = true;
         openssh.authorizedKeys.keys = [
-          "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBADbQB9eZMIYo0l+CGxMnqNuWjCVqUhEKFOpWpJG0OqNkiqvozTcSYOu8MggCjDOFRzxn4rnN3Tyzb/gtXZREBTCFAF8ZBymZ4ZJYfFZn0C5vqI2szGrHRbhcu/YMo6aJwxbkQFZ2leXkyCKYCXuFYTtNCu1MxBw3iDn2MlGjK2FH0m3dA== ceeser"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkBzlg0udbPMz8wquIXjFMeHnLrsONMWLWltKpFgXik"
         ];
       };
